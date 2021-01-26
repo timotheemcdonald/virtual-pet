273755300e18af3ea2b34d237b09cb7a214d8bf2
timotheemcdonald <timotheemcdonald@gmail.com>
2021-01-25 21:46:58 -0500

    set up tamagotchi shell and vue router

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <GigaVue />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import GigaVue from '@/components/GigaVue.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    GigaVue
+  }
+}
+</script>
