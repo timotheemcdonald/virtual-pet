1bb466775eb0bd7ec46cd9790f1d0c336469ec4d
timotheemcdonald <timotheemcdonald@gmail.com>
2021-01-25 22:26:43 -0500

    pet shows after being named

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,13 @@
 <script>
 // @ is an alias to /src
 import {reactive, toRefs} from 'vue'
+import DinoPet from '@/components/DinoPet.vue'
 import GigaVue from '@/components/GigaVue.vue'
 
 export default {
   name: 'Home',
   components: {
+    DinoPet,
     GigaVue
   },
   setup(){
@@ -31,7 +33,10 @@ export default {
 <template>
   <div class="home">
     <h1>{{ petName}}</h1>
+    <section class="wrapper">
     <GigaVue />
+    <DinoPet class="dinopet" v-if="petName" />
+    </section>
     <form v-if="showCreatePetForm" @submit.prevent>
     <label for="pet-name">Pet Name</label>
     <input type="text" id="pet-name" v-model="userInput" />
@@ -40,4 +45,15 @@ export default {
   </div>
 </template>
 
+<style scoped>
+.wrapper {
+  position: relative;
+}
 
+.dinopet {
+  max-width: 120px;
+  position: absolute;
+  left: 700px;
+  top: 210px;
+}
+</style>
